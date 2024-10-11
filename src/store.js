5e1bcad0ba7b73afcd8b3e751451931bc146e5c0
Yohaglis Vivenes <yohaglisvivenes@gmail.com>
2024-10-11 14:36:50 -0400

    se agregan reductores para poder usarlos en la lista de productos

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import cartReducer from './CartSlice';
 
  const store = configureStore({
     reducer: {
-        cart: cartReducer,
+        cart: cartReducer,        
     },
 });
 export default store

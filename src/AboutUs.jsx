2d5c7ac656d0c9d53b41bbfdcf4fc8fc47efb26c
Yohaglis Vivenes <yohaglisvivenes@gmail.com>
2024-10-12 12:10:10 -0400

    se agrega logica para deshabilitar el boton un avez se agrega un productos

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -5,24 +5,18 @@ function AboutUs() {
   return (
     <div className="about-us-container">
       {/* <h1 className="about-us-heading">About Us</h1> */}
-      <p className="about-us-description">Welcome to Paradise Nursery, where green meets serenity!</p>
+      <p className="about-us-description">Welcome to Forest Plants Company, the freshness of the wild in your garden!</p>
       <p className="about-us-content">
-        At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is to provide a wide range of 
-        high-quality plants that not only enhance the beauty of your surroundings but also contribute to a healthier and 
-        more sustainable lifestyle. From air-purifying plants to aromatic fragrant ones, we have something for every 
-        plant enthusiast.
+        The Plants provide health benefits, purify the air, connect us with nature and boost our creativity, generating well-being in every space we inhabit. We advise you to create green spaces.
       </p>
       {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
       <p className="about-us-content">
-        Our team of experts is dedicated to ensuring that each plant meets our strict standards of quality and care. 
-        Whether you're a seasoned gardener or just starting your green journey, we're here to support you every step of 
-        the way. Feel free to explore our collection, ask questions, and let us help you find the perfect plant for your 
-        home or office.
+        At Biotienda, we offer you the opportunity to give exceptional plants with scheduled delivery. "Personalize your green gift! From choosing the pot to adding special messages, create a unique gift that lasts.
       </p>
       {/* <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
 
       <p className="about-us-content">
-        Join us in our mission to create a greener, healthier world. Visit Paradise Nursery today and experience the 
+        Join us in our mission to create a greener, healthier world. Visit Forest Plants Company today and experience the 
         beauty of nature right at your doorstep.
       </p>
     </div>
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -280,8 +280,8 @@ function ProductList() {
                         <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
                         <a href="/" style={{ textDecoration: 'none', marginLeft: '20px' }}>
                             <div>
-                                <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
-                                <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
+                                <h3 style={{ color: 'white' }}>Forest Plants Company</h3>
+                                <i style={{ color: 'white' }}>The freshness of the wild in your garden</i>
                             </div>
                         </a>
                     </div>

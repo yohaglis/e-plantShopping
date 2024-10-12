f219947432f0dd7553fadf27e2cc5f086a880b99
Yohaglis Vivenes <yohaglisvivenes@gmail.com>
2024-10-11 20:44:12 -0400

    avances

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,39 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    const totalAmount = cart.reduce((acc, item) => {
+        return acc + item.quantity * item.cost;
+    }, 0);
+    return totalAmount;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
-
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));  
   };
 
   const handleDecrement = (item) => {
-   
+   if(item.length > 0){
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 })); 
+   }else{
+    dispatch(removeItem(item));
+   }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.cost * item.quantity;
+  };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
   return (
